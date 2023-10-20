36176072c2af916a83fce8b99fe5b11058f88761
tua2542 <sakdipat3536@gmail.com>
2023-10-20 08:55:44 +0700

    Create SaleByMatch.js

---
+++ Warm-up Challenges/SaleByMatch.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'sockMerchant' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER_ARRAY ar
+ */
+
+function sockMerchant(n, ar) {
+    let numSock = n - 1;
+    let sortArr = ar.sort((a,b) => a - b);
+    let pair = 0;
+    for(let i = 0; i < numSock; i++){
+        if(sortArr[i] === sortArr[i+1]){
+            pair = pair + 1;
+            /*Skip the paired index*/
+            i = i + 1;
+        }
+    }
+    return pair;
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const ar = readLine().replace(/\s+$/g, '').split(' ').map(arTemp => parseInt(arTemp, 10));
+
+    const result = sockMerchant(n, ar);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
