857a97dd2cda3edb02a1b9aa91ef41568513bd08
Sakdipat Ontoum <sakdipat3536@gmail.com>
2023-12-05 21:39:27 +0700

    [ADD] climbingLeaderboard.js

---
+++ Algorithms/Implementation/ClimbingLeaderboard.js
@@ -0,0 +1,66 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function (inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function () {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'climbingLeaderboard' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY ranked
+ *  2. INTEGER_ARRAY player
+ */
+
+function climbingLeaderboard(ranked, player) {
+    ranked = [...new Set(ranked)];
+    ranked.sort((a, b) => b - a); //descending
+    let n = ranked.length;
+    let results = [];
+    for (let score of player) {
+
+        while (n > 0 && ranked[n - 1] <= score) {
+            n = n - 1;
+        }
+        results.push(n + 1);
+    }
+
+    return results;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const rankedCount = parseInt(readLine().trim(), 10);
+
+    const ranked = readLine().replace(/\s+$/g, '').split(' ').map(rankedTemp => parseInt(rankedTemp, 10));
+
+    const playerCount = parseInt(readLine().trim(), 10);
+
+    const player = readLine().replace(/\s+$/g, '').split(' ').map(playerTemp => parseInt(playerTemp, 10));
+
+    const result = climbingLeaderboard(ranked, player);
+
+    ws.write(result.join('\n') + '\n');
+
+    ws.end();
+}
