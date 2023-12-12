d5d720569b9efbb69006e916fe4d2b77a3cd9ce3
Sakdipat Ontoum <sakdipat3536@gmail.com>
2023-12-12 20:19:37 +0700

    [ADD] findDigit and JumpOnCloud

---
+++ Algorithms/Implementation/findDigits.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'findDigits' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER n as parameter.
+ */
+
+function findDigits(n) {
+    let digits = n.toString().split("");
+    let count = 0;
+    for(let digit of digits){
+        if(n % digit === 0 && digit !== 0){
+            count = count + 1;
+        }
+    }
+    return count;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const t = parseInt(readLine().trim(), 10);
+
+    for (let tItr = 0; tItr < t; tItr++) {
+        const n = parseInt(readLine().trim(), 10);
+
+        const result = findDigits(n);
+
+        ws.write(result + '\n');
+    }
+
+    ws.end();
+}
---
+++ Algorithms/Implementation/jumpOnCloudRevisited.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the jumpingOnClouds function below.
+function jumpingOnClouds(c, k) {
+    const n = c.length;
+    let energy = 100;
+    let currentPosition = 0;
+
+    do {
+        currentPosition = (currentPosition + k) % n;
+        energy -= c[currentPosition] === 1 ? 3 : 1;
+    } while (currentPosition !== 0);
+
+    return energy;
+}
+
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const nk = readLine().split(' ');
+
+    const n = parseInt(nk[0], 10);
+
+    const k = parseInt(nk[1], 10);
+
+    const c = readLine().split(' ').map(cTemp => parseInt(cTemp, 10));
+
+    let result = jumpingOnClouds(c, k);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
