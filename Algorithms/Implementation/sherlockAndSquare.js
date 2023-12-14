9c7501ab0b0b098971b7e132359d0ed447f8fb0b
Sakdipat Ontoum <sakdipat3536@gmail.com>
2023-12-14 20:49:13 +0700

    [ADD] libraryFine and Sherlock

---
+++ Algorithms/Implementation/sherlockAndSquare.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'squares' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER a
+ *  2. INTEGER b
+ */
+
+function squares(a, b) {
+    let count = 0;
+    let x  = Math.ceil(Math.sqrt(a));
+    while(Math.pow(x, 2) <= b){
+        count = count + 1;
+        x = x + 1;
+    }
+    return count;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const q = parseInt(readLine().trim(), 10);
+
+    for (let qItr = 0; qItr < q; qItr++) {
+        const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+        const a = parseInt(firstMultipleInput[0], 10);
+
+        const b = parseInt(firstMultipleInput[1], 10);
+
+        const result = squares(a, b);
+
+        ws.write(result + '\n');
+    }
+
+    ws.end();
+}
