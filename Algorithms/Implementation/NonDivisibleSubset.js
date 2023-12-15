4fa1462f0b5f916efa094557585761cf689e44d5
Sakdipat Ontoum <sakdipat3536@gmail.com>
2023-12-15 19:59:13 +0700

    [ADD] cutTheSticks and NonDivislble

---
+++ Algorithms/Implementation/NonDivisibleSubset.js
@@ -0,0 +1,68 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'nonDivisibleSubset' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER k
+ *  2. INTEGER_ARRAY s
+ */
+
+function nonDivisibleSubset(k, s) {
+    let remainders  = Array(k).fill(0);
+    for(let i = 0; i < s.length; i++){
+      let remainder = s[i] % k;
+      remainders[remainder]++;  
+    }
+     let count = Math.min(remainders[0], 1);
+     for(let i = 1; i <= Math.floor(k / 2); i++){
+         if(i !== k - i){
+             count += Math.max(remainders[i], remainders[k - i]);
+         }
+     }
+     if(k % 2 === 0){
+         count++;
+     }
+     return count;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+    const n = parseInt(firstMultipleInput[0], 10);
+
+    const k = parseInt(firstMultipleInput[1], 10);
+
+    const s = readLine().replace(/\s+$/g, '').split(' ').map(sTemp => parseInt(sTemp, 10));
+
+    const result = nonDivisibleSubset(k, s);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
