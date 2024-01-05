01d06874ed547e1868a1db45d626b3ab96d38e6f
Sakdipat Ontoum <sakdipat3536@gmail.com>
2024-01-05 20:07:48 +0700

    [ADD] camelCase and superRedStr

---
+++ Algorithms/string/superReducedStr.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'superReducedString' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+function superReducedString(s) {
+    let result = [];
+    
+    for(let i = 0; i < s.length; i++){
+        if(result.length > 0 && result[result.length - 1] === s[i]){
+            result.pop();
+        } else {
+            result.push(s[i]);
+        }
+    }
+    
+    return result.length === 0 ? 'Empty String' : result.join('');
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const s = readLine();
+
+    const result = superReducedString(s);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
