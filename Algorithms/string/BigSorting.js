f9f45694c0b2eeba60830f6507fd97c3600d5cd0
Sakdipat Ontoum <sakdipat3536@gmail.com>
2024-01-08 20:48:34 +0700

    [ADD] bigSorting

---
+++ Algorithms/string/BigSorting.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'bigSorting' function below.
+ *
+ * The function is expected to return a STRING_ARRAY.
+ * The function accepts STRING_ARRAY unsorted as parameter.
+ */
+
+function bigSorting(unsorted) {
+    return unsorted.sort((a, b) => {
+        if(a.length !== b.length){
+            return a.length - b.length;
+        } else {
+            return a.localeCompare(b);
+        }
+    });
+    return unsorted;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    let unsorted = [];
+
+    for (let i = 0; i < n; i++) {
+        const unsortedItem = readLine();
+        unsorted.push(unsortedItem);
+    }
+
+    const result = bigSorting(unsorted);
+
+    ws.write(result.join('\n') + '\n');
+
+    ws.end();
+}
