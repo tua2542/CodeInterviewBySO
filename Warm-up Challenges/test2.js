520e08919885a971014e09cd39cf6932f27f6ccd
Sakdipat Ontoum <sakdipat3536@gmail.com>
2024-02-12 22:05:20 +0700

    [ADD] test2

---
+++ Warm-up Challenges/test2.js
@@ -0,0 +1,12 @@
+function showListNumWithPow (n) {
+    let num = 1;
+    for (let i = num; i <= n; i++) {
+        if(Number.isInteger(Math.sqrt(i))){
+            console.log("POWER");
+        } else {
+            console.log(i);
+        }
+    }
+}
+
+showListNumWithPow(16);
